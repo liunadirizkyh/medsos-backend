a96b59690cb7273caec1ca10c3b4f7038b5db118
Liunadi Rizky Hidayat <liunadirizky89@gmail.com>
2026-03-05 04:47:07 +0700

    feat: add cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import authRouter from "./routes/auth.route.js";
 import "dotenv/config";
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
