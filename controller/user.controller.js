436d93a5ebc1f831313f327d9ab73809a4673016
liu <liunadi.hidayat@binus.ac.id>
2026-03-13 13:24:17 +0700

    feat: add get search user by query params

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -25,3 +25,39 @@ export const getUserbyUsername = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const getSearchUser = async (req, res) => {
+  const { username } = req.query;
+
+  if (!username) {
+    return res.status(404).json({ error: "Username query is required" });
+  }
+
+  try {
+    const user = await prisma.user.findMany({
+      where: {
+        username: {
+          contains: username,
+          mode: "insensitive",
+        },
+      },
+      select: {
+        id: true,
+        username: true,
+        fullname: true,
+        image: true,
+      },
+    });
+
+    if (user.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "User retrieved successfully", user: user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getUserbyUsername } from "../controller/user.controller.js";
+import {
+  getSearchUser,
+  getUserbyUsername,
+} from "../controller/user.controller.js";
 
 const userRouter = Router();
 
+userRouter.get("/search", getSearchUser);
 userRouter.get("/:username", getUserbyUsername);
 
 export default userRouter;
