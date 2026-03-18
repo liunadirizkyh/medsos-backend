af865dfe20ee4b19470889c984902de504e50049
liu <liunadi.hidayat@binus.ac.id>
2026-03-18 13:35:44 +0700

    feat: add unlike post

--- controller/like.controller.js
+++ controller/like.controller.js
@@ -25,9 +25,27 @@ export const createLike = async (req, res) => {
     });
 
     if (alreadyLike) {
-      return res
-        .status(400)
-        .json({ message: "You have already liked this post" });
+      await prisma.likes.delete({
+        where: {
+          userId_postId: {
+            userId: currentUser,
+            postId: id,
+          },
+        },
+      });
+
+      await prisma.post.update({
+        where: {
+          id,
+        },
+        data: {
+          likeCount: {
+            decrement: 1,
+          },
+        },
+      });
+
+      return res.status(200).json({ message: "Post unliked successfully" });
     }
 
     await prisma.likes.create({
@@ -52,5 +70,40 @@ export const createLike = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const checkLike = async (req, res) => {
+  const id = Number(req.params.id);
+  const currentUser = req.user.id;
+
+  try {
+    const feed = await prisma.post.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!feed) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const like = await prisma.likes.findUnique({
+      where: {
+        userId_postId: {
+          userId: currentUser,
+          postId: id,
+        },
+      },
+    });
+
+    if (like) {
+      return res.status(200).json({ data: true });
+    } else {
+      return res.status(200).json({ data: false });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error", error });
   }
 };
--- routes/like.route.js
+++ routes/like.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
-import { createLike } from "../controller/like.controller.js";
+import { checkLike, createLike } from "../controller/like.controller.js";
 
 const likeRouter = Router();
 
 likeRouter.post("/:id", authMiddleware, createLike);
+likeRouter.get("/:id", authMiddleware, checkLike);
 
 export default likeRouter;
